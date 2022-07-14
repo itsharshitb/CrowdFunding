44e327e19d4b1a524de705117ff64aa78d37826a
"Harshit Bhatt" <"harshitbhatt972@gmail.com">
2022-07-14 11:47:38 +0530

    "User side created, creating manager side"

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -11,6 +11,17 @@ contract CrowdFunding{
     uint public raisedAmount;
     uint public noOfContributors;  // total contribtr
 
+    struct Request{
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voters;  //keep track of voters for yes/no        
+    }
+    mapping(uint=>Request) requests;    //map each request with int
+    uint public numRequests; //count total request made
+
     constructor(uint _target, uint _deadline){  //set by the manager initially while deploying
         target=_target;
         deadline=block.timestamp+ _deadline;
@@ -31,5 +42,13 @@ contract CrowdFunding{
 
     function getContractBalance() public view returns(uint){
         return address(this).balance;
+    }
+
+    function refund() public{
+        require(block.timestamp>deadline && target>raisedAmount,"You are not eligible for refund");
+        require(contributors[msg.sender]>0,"You haven't contributed yet");
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender]=0;
     }
 }
